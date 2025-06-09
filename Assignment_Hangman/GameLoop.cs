11be1d099b58385db0f32809f6f8d30c6771274d
a21jeaha <hisui86@gmail.com>
2025-06-09 08:36:19 +0200

    implemented validation of secret word

--- Assignment_Hangman/GameLoop.cs
+++ Assignment_Hangman/GameLoop.cs
@@ -1,16 +1,38 @@
 ﻿
+
 namespace Assignment_Hangman
 {
 	internal class GameLoop
 	{
-		public GameLoop()
-		{
-			
+		private string _secretWord = string.Empty;
+		public string SecretWord 
+		{ 
+			get => _secretWord;
+			private set 
+			{
+				if (IsValidWord(value))
+					_secretWord = value;
+			} 
 		}
 
-		internal void PrepareNewSecretWord(string v)
+		private bool IsValidWord(string value)
 		{
-			throw new NotImplementedException();
+			if (string.IsNullOrWhiteSpace(value))
+			{
+				// ToDo: Handle this exception.
+				throw new ArgumentNullException(nameof(value), "Secret word cannot be null or empty.");
+			}
+			else if (value.Length > 18) 
+			{
+				throw new ArgumentOutOfRangeException(nameof(value), "Secret word cannot be longer then 18 characters.");
+			}
+
+			return true;
+		}
+
+		internal void PrepareNewSecretWord(string secretWord)
+		{
+			SecretWord = secretWord;
 		}
 	}
 }
